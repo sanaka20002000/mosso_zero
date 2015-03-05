2163d099bb4db0dd7de9825f13f44006b8043715
AHA03953 <AHA03953@9.170.124.190>
2015-03-05 22:41:28 +0900

    0.4 bootstrap

---
+++ Project_Mosso/cms/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+from cms import views
+
+urlpatterns = patterns('',
+    url(r'^book/$', views.book_list, name='book_list'),   
+    url(r'^book/add/$', views.book_edit, name='book_add'),  
+    url(r'^book/mod/(?P<book_id>\d+)/$', views.book_edit, name='book_mod'),  
+    url(r'^book/del/(?P<book_id>\d+)/$', views.book_del, name='book_del'),
+)
